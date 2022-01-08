bc6c5bf694be2f4b1aacb6d402ccf3242e4b63fe
Filippo Musso <fili.musso05@gmail.com>
2022-01-08 21:44:39 +0100

    Created intro animation with javascript

---
+++ app.js
@@ -0,0 +1,27 @@
+let intro = document.querySelector(".intro");
+let text = document.querySelector(".text-header");
+let textSpan = document.querySelectorAll(".text");
+let textAfter = document.querySelector(".text-after")
+
+window.addEventListener('DOMContentLoaded', ()=>{
+  setTimeout(()=>{
+    textSpan.forEach((span, idx)=>{
+      setTimeout(()=>{
+        span.classList.add("active");
+      }, (idx + 1) * 400)
+    });
+
+    setTimeout(()=>{
+      textSpan.forEach((span, idx)=>{
+        setTimeout(()=>{
+          span.classList.remove("active");
+          span.classList.add("fade");
+        }, (idx + 1) * 50)
+      })
+    }, 2000);
+
+    setTimeout(()=>{
+      intro.style.top = "-100vh";
+    }, 2300)
+  })
+})
